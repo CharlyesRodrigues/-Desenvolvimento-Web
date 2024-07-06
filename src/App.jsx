740fd3194b13ece7461001184eeebe0d49a9c70a
Charlyes <charlyessouza@gmail.com>
2024-07-05 21:06:54 -0300

    Iniciando o Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p >
+      teste
       </p>
       
       
